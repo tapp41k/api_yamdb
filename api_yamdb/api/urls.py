e1628f372da26b48588b824509611a4b15fbc6cd
Анна Митрофанова <av.kashirina@gmail.com>
2023-05-27 21:24:05 +0300

    исправлена модель, файл зависимостей, импорт

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -18,6 +18,7 @@ v1_router.register(r'titles/(?P<title_id>\d+)/reviews',
 v1_router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
                    r'/comments', CommentViewSet, basename='comments')
 
+
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path(
